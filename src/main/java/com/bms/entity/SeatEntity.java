14a81b2c28f73b53fdbeb77c6e5f6d5a040471ea
Sunil Kumar <sunilkumar.santro@gmail.com>
2023-09-19 20:17:44 +0530

    adding version for optimistic locking

--- src/main/java/com/bms/entity/SeatEntity.java
+++ src/main/java/com/bms/entity/SeatEntity.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 
 @Entity
 @Table(name = "Seat")
@@ -25,6 +26,9 @@ public class SeatEntity {
 
 	@Column
 	private String seatType;
+	
+    @Version
+    private Long version;
 
 	@ManyToOne
 	@JoinColumn(name = "screenID", nullable = false)
